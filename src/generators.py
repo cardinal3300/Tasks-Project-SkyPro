7616955ca8342ccb3f73b3324af32930620dbf90
Zherdev Igor <cardinal3300@.com>
2025-06-13 10:08:46 +0300

    add generators, test_generators and fix README

---
+++ src/generators.py
@@ -0,0 +1,37 @@
+from typing import List, Dict, Iterator, Any
+
+def filter_by_currency(
+    transactions: List[Dict[str, Any]], currency: str
+) -> Iterator[Dict[str, Any]]:
+    """ Возвращает итератор только по тем транзакциям, где валюта соответствует заданной. """
+    # Валидация входных данных.
+    if not isinstance(currency, str):
+        raise TypeError("currency должен быть строкой.")
+    for transaction in transactions:
+        operation_amount = transaction.get("operationAmount")
+        if operation_amount:
+            currency_info = operation_amount.get("currency")
+            if currency_info:
+                if currency_info.get("code") == currency:
+                    yield transaction
+
+
+def transaction_descriptions(
+    transactions: List[Dict[str, Any]]
+) -> Iterator[str]:
+    """ Генерирует описания транзакций из списка словарей с транзакциями. """
+    for transaction in transactions:
+        description = transaction.get("description")
+        if description:
+            yield description
+
+
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    """ Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX в заданном диапазоне. """
+    for number in range(start, stop + 1):
+        card_number_str = str(number)
+    # Дополнение нулями слева до 16 символов
+        if len(card_number_str) < 16:
+            card_number_str = "0" * (16 - len(card_number_str)) + card_number_str
+            formatted_card_number = f"{card_number_str[0:4]} {card_number_str[4:8]} {card_number_str[8:12]} {card_number_str[12:]}"
+            yield formatted_card_number
